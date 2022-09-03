759727fa443e0824a4ee6ab5c65f15e8e73b7aa1
codingtyro <2582678333@qq.com>
2022-09-03 22:57:03 +0800

    Fibonacci数列

---
+++ 算法/递归/Fibonacci数列/Main.java
@@ -0,0 +1,12 @@
+/*
+ * 无穷数列1,1,2,3,5,8,13,21,34.....被称为Fibonacci数列。
+ */
+
+public class Main {
+    public int fibonacci(int n){
+        if(n <= 1){
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
