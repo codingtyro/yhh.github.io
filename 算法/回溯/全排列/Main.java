6f79d85dbc897ddf48a4582b5ed91520d4b380e1
luo <3250126125@qq.com>
2022-09-11 16:36:19 +0800

    加注释

--- 算法/回溯/全排列/Main.java
+++ 算法/回溯/全排列/Main.java
@@ -17,6 +17,7 @@ class Main{
             for(int i = k;i <= m;i++){
                 swap(list, k, i);
                 perm(list, k + 1, m);
+                //1
                 swap(list, k, i);
             }
         }
